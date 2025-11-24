e292c2d89ecbc4027df1da1cb76c5dbc4686fe19
Krishna Pandey <krishnapand0727@gmail.com>
2025-11-24 16:56:49 +0530

    Refactor circular list display and cleanup code

--- Assignment 6/q2.cpp
+++ Assignment 6/q2.cpp
@@ -6,8 +6,6 @@ struct Node{ int data; Node* next; Node(int x): data(x), next(NULL) {} };
 int main(){
     int n; cout<<"Enter n: "; cin>>n;
     if(n<=0){ cout<<"\n"; return 0; }
-
-    // build circular list from input
     Node *head=NULL,*tail=NULL;
     for(int i=0;i<n;i++){
         int x; cin>>x;
@@ -15,14 +13,10 @@ int main(){
         if(!head){ head=tail=t; }
         else { tail->next=t; tail=t; }
     }
-    tail->next=head; // make circular
-
-    // display with head value again at end
+    tail->next=head; 
     Node* p=head;
     do{ cout<<p->data<<' '; p=p->next; } while(p!=head);
     cout<<head->data<<'\n';
-
-    // free
     p=head->next; while(p!=head){ Node* t=p; p=p->next; delete t; } delete head;
     return 0;
 }
